ace8a1eeff497a6e173fbce055ddb8f8a572b8ef
Michael Callahan <CallahanMichaelE@gmail.com>
2020-03-18 13:44:37 -0600

    added jokeReducer for skeleton of reducer as well as a guess at inital state. Also imported needed dependencies and set up index.js in src with store apply middleware, thunk, and the provider. we should be ready to move on to the next steps. Also decided on a joke API.

---
+++ my-app/src/reducers/jokeReducer.js
@@ -0,0 +1,16 @@
+
+
+const initialState = {
+    jokeData: [],
+    isFetchingData: false,
+    error: ''
+};
+
+export const jokerReducer = (state=initialState, action) => {
+    switch (action.type) {
+
+
+        default:
+            return state;
+    }
+};
